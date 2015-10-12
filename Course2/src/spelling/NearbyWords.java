4c392cf76e8d7c45f28cbd410cdd1f91e50baf44
Leo Porter <leporter@eng.ucsd.edu>
2015-10-12 11:22:13 -0700

    Second push of NearbyWords.

--- Course2/src/spelling/NearbyWords.java
+++ Course2/src/spelling/NearbyWords.java
@@ -14,7 +14,7 @@ import java.util.List;
  *
  */
 public class NearbyWords implements SpellingSuggest {
-    private static final int THRESHOLD = 1000;
+    private static final int THRESHOLD = 1000; 
 	
 	Dictionary dict;
    
